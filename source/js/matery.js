3c321ac607ead78a8fd436858ca6354a09726ff6
blinkfox <chenjiayin1990@163.com>
2018-09-09 00:34:34 +0800

    去掉主题设置和切换的页面，增加国际化的支持

--- source/js/matery.js
+++ source/js/matery.js
@@ -1,54 +1,3 @@
-/*调色面板的开关方法*/
-function openOrClosePanel() {
-    var l = $('#theme-change').attr('left');
-    if (l == -200) {
-        $('#theme-change').attr('left', 0).animate({'left': '0'});
-    } else {
-        $('#theme-change').attr('left', -200).animate({'left': '-200px'});
-    }
-}
-
-/*设置主题时替换样式方法*/
-function replaceClass(color, c) {
-    $('.' + c + 'color').each(function() {
-        var newCls = '';
-        var cls =  $(this).attr('class').split(' ');
-        /*遍历去掉老的颜色值*/
-        for (var i = 0; i < cls.length; i++) {
-            var cl = cls[i];
-            if ((cl.indexOf(c) < 0) || ((c + 'color') == cl)) {
-                newCls = (i == cls.length - 1) ? newCls + cl : newCls + cl + ' ';
-            }
-        }
-        newCls = $.trim(newCls);
-        $(this).attr('class', newCls);
-        $(this).addClass(c + color);
-    });
-}
-
-function changeTheme(t) {
-    var color = t.attr('data-colors');
-    replaceClass(color, 'bf-');
-    replaceClass(color, 'bf-text-');
-    setLocalTheme(color);
-}
-
-/*设置本地颜色到localStorage中*/
-function setLocalTheme(color) {
-    if (color) {
-        window.localStorage.setItem('bfTheme', color);
-    }
-}
-
-/*初始化主题颜色*/
-function initTheme() {
-    var color = window.localStorage.getItem('bfTheme');
-    if (color) {
-        replaceClass(color, 'bf-');
-        replaceClass(color, 'bf-text-');
-    }
-}
-
 /*设置所有文章div的宽度*/
 function setArtWidth() {
     var w = $('#navContainer').width();
@@ -140,18 +89,6 @@ $(function() {
     /*菜单切换*/
     $('.button-collapse').sideNav();
 
-    /*初始化主题*/
-    initTheme();
-
-    /*silider初始化,由于默认禁用了silider效果，这里先注释掉*/
-    /*
-    $('.slider').slider({
-        height: 230,
-        interval: 5000
-    });
-    $('#featureSlider .slider').height(260);
-    */
-
     setArtWidth();
     fixFooterPosition();
 
@@ -178,27 +115,6 @@ $(function() {
         $('#searchModal').openModal();
     });
 
-    /*调色面板显示开关*/
-    $('#theme-change .theme-change-handle').click(function() {
-        openOrClosePanel();
-    });
-    /*移动端主题设置的显示开关*/
-    $('#themeHandle').click(function() {
-        $('#themeModal').openModal();
-        $('.button-collapse').sideNav('hide');
-    });
-    /*桌面端调色面板某颜色选中样式时的主题颜色设置*/
-    $('#theme-change .theme-change-container ul li').click(function() {
-        $(this).addClass('selected').siblings().removeClass('selected');
-        changeTheme($(this));
-        openOrClosePanel();
-    });
-    /*移动端调色面板某颜色选中样式时的主题颜色设置*/
-    $('#themeModal .theme-settings div').click(function() {
-        changeTheme($(this));
-        $('#themeModal').closeModal();
-    });
-
     /*回到顶部*/
     $('#backTop').click(function() {
         $('body,html').animate({scrollTop: 0}, 400);
@@ -207,19 +123,12 @@ $(function() {
 
     /*监听滚动条位置*/
     $(window).scroll(function(event) {
-        var t = $(window).scrollTop();
-        /*导航栏位置监听*/
-        var nav = $('#headNav');
-        if (t < 250) {
-            nav.addClass('nav-transparent');
-        } else {
-            nav.removeClass('nav-transparent');
-        }
-
         /*回到顶部按钮根据滚动条的位置的显示和隐藏*/
-        if (t < 100) {
+        if ($(window).scrollTop() < 100) {
+            $('#headNav').addClass('nav-transparent');
             $('.top-scroll').slideUp(300);
         } else {
+            $('#headNav').removeClass('nav-transparent');
             $('.top-scroll').slideDown(300);
         }
     });
