e379a1fd3b6989cfd438d485efd42cdc77c186bf
blinkfox <chenjiayin1990@163.com>
2018-09-22 01:02:21 +0800

    修改大多数文件的代码格式，新增了my.css文件，用于书写自己的样式信息

--- source/js/matery.js
+++ source/js/matery.js
@@ -1,12 +1,12 @@
-$(function() {
+$(function () {
     /**
      * 添加文章卡片hover效果.
      */
-    let articleCardHover = function() {
+    let articleCardHover = function () {
         let animateClass = 'animated pulse';
-        $('#articles .article, #tags .article').hover(function() {
+        $('#articles .article, #tags .article').hover(function () {
             $(this).addClass(animateClass);
-        }, function() {
+        }, function () {
             $(this).removeClass(animateClass);
         });
     };
@@ -15,12 +15,12 @@ $(function() {
     /**
      * 根据标签切换文章.
      */
-    let changeTagPostByTag = function(tag) {
+    let changeTagPostByTag = function (tag) {
         $('#tags .chip-active').removeClass('chip-active').addClass('chip-default');
         $('#tags .tag-chips span[data-tagname="' + tag + '"]').removeClass('chip-default').addClass('chip-active');
 
         // 获取同样有该标签的帖子，并将其显示出来，其他帖子隐藏.
-        $('#tags .tag-post').each(function() {
+        $('#tags .tag-post').each(function () {
             let tagArr = $(this).attr('data-tags').split(', ');
             if ($.inArray(tag, tagArr) >= 0) {
                 $(this).fadeIn();
@@ -33,8 +33,8 @@ $(function() {
     /**
      * 切换标签时的帖子显示情况.
      */
-    let changeTagPost = function() {
-        $('#tags .chip').click(function() {
+    let changeTagPost = function () {
+        $('#tags .chip').click(function () {
             // 如果当前标签已经激活了，则直接return.
             if ($(this).hasClass('chip-active')) {
                 return;
@@ -44,7 +44,7 @@ $(function() {
             changeTagPostByTag($(this).text());
         });
     };
-    
+
     /* 切换标签帖子. */
     let tagAnchor = decodeURI(window.location.hash);
     if (tagAnchor.indexOf('#') >= 0) {
@@ -56,7 +56,7 @@ $(function() {
     $('.button-collapse').sideNav();
 
     /*设置所有文章div的宽度*/
-    let setArtWidth = function() {
+    let setArtWidth = function () {
         let w = $('#navContainer').width();
         if (w >= 450) {
             w = w + 21;
@@ -73,7 +73,7 @@ $(function() {
     /**
      * 修复footer部分的位置，使得在内容比较少时，footer也会在底部.
      */
-    let fixFooterPosition = function() {
+    let fixFooterPosition = function () {
         $('.content').css('min-height', window.innerHeight - 165);
     };
 
@@ -81,7 +81,7 @@ $(function() {
     fixFooterPosition();
 
     /*调整屏幕宽度时重新设置文章列的宽度，修复小间距问题*/
-    $(window).resize(function() {
+    $(window).resize(function () {
         setArtWidth();
         fixFooterPosition();
     });
@@ -98,10 +98,10 @@ $(function() {
     });
 
     /*文章内容详情的一些初始化特性*/
-    let articleInit = function() {
+    let articleInit = function () {
         $('#articleContent a').attr('target', '_blank');
 
-        $('#articleContent img').each(function() {
+        $('#articleContent img').each(function () {
             let imgPath = $(this).attr('src');
             $(this).wrap('<div class="img-item" data-src="' + imgPath + '"></div>');
         });
@@ -117,18 +117,18 @@ $(function() {
     };
     articleInit();
 
-    $('#toggleSearch').click(function() {
+    $('#toggleSearch').click(function () {
         $('#searchModal').openModal();
     });
 
     /*回到顶部*/
-    $('#backTop').click(function() {
+    $('#backTop').click(function () {
         $('body,html').animate({scrollTop: 0}, 400);
         return false;
     });
 
     /*监听滚动条位置*/
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         /*回到顶部按钮根据滚动条的位置的显示和隐藏*/
         if ($(window).scrollTop() < 100) {
             $('#headNav').addClass('nav-transparent');
