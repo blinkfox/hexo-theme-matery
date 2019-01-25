004054b0f471522c0281d21b209e7dd300610fa3
blinkfox <chenjiayin1990@163.com>
2019-01-25 22:44:38 +0800

    让图片居中，修改了两个样式的命名

--- source/js/matery.js
+++ source/js/matery.js
@@ -76,7 +76,7 @@ $(function () {
             let imgPath = $(this).attr('src');
             $(this).wrap('<div class="img-item" data-src="' + imgPath + '" data-sub-html=".caption"></div>');
             // 图片添加阴影
-            $(this).addClass("img_shadow img_margin");
+            $(this).addClass("img-shadow img-margin");
             // 图片添加字幕
             let alt = $(this).attr('alt');
             let title = $(this).attr('title');
