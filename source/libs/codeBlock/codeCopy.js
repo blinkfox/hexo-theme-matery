80e9c1347abc3b78291e1ca9cce867c4142c5eaa
Licardo <1014660822@qq.com>
2019-10-14 00:33:10 +0800

    Update codeCopy.js

--- source/libs/codeBlock/codeCopy.js
+++ source/libs/codeBlock/codeCopy.js
@@ -4,22 +4,14 @@ $(function () {
    */
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>')
   $('pre').before($highlight_lang)
-  var lang_name_index
   var lang_name
   $('pre').each(function () {
-    lang_name_index = lang_name = $('code').attr('class').split('-')[1];
-    if (lang_name_index == 'js')
-      lang_name = 'javascript'
-    if (lang_name_index == 'md')
-      lang_name = 'markdown'
-    if (lang_name_index == 'plain')
-      lang_name = 'code'
-    if (lang_name_index == 'py')
-      lang_name = 'python'
+    lang_name = $('code').attr('class').split('-')[1];
 
     // 首字母大写
-    lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1)
+    lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
 
     $('pre').siblings(".code_lang").text(lang_name)
   })
+
 });
