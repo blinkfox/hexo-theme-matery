89921e301150cab7955b7ef0175f8753064c5a08
Licardo <1014660822@qq.com>
2019-10-13 22:01:10 +0800

    Update codeCopy.js

--- source/libs/codeBlock/codeCopy.js
+++ source/libs/codeBlock/codeCopy.js
@@ -4,7 +4,7 @@ $(function () {
      * Add copy icon
      */
     $('.line-numbers').wrap('<div class="code-area" style="position: relative"></div>')
-    var $copyIcon = $('<i class="fas fa-copy" title="复制代码" style="position: absolute; top: 0.7rem; right: 10px; z-index: 1; filter: invert(50%); cursor: pointer;" aria-hidden="true"></i>')
+    var $copyIcon = $('<i class="fas fa-copy code_copy" title="复制代码" aria-hidden="true"></i>')
     $('.code-area').prepend($copyIcon)
     new ClipboardJS('.fa-copy', {
         target: function(trigger) {
