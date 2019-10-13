532923fcdf1530d8d554525141d960de6cb5d1ae
Licardo <1014660822@qq.com>
2019-10-14 00:18:25 +0800

    Update codeCopy.js

--- source/libs/codeBlock/codeCopy.js
+++ source/libs/codeBlock/codeCopy.js
@@ -1,14 +1,25 @@
 $(function () {
     /**
-     * 代码复制按钮
-     * Add copy icon
-     */
-    $('.line-numbers').wrap('<div class="code-area" style="position: relative"></div>')
-    var $copyIcon = $('<i class="fas fa-copy code_copy" title="复制代码" aria-hidden="true"></i>')
-    $('.code-area').prepend($copyIcon)
-    new ClipboardJS('.fa-copy', {
-        target: function(trigger) {
-            return trigger.nextElementSibling;
-        }
-    })
+   * 代码框语言识别
+   */
+  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>')
+  $('pre').before($highlight_lang)
+  var lang_name_index
+  var lang_name
+  $('pre').each(function () {
+    lang_name_index = lang_name = $('code').attr('class').split('-')[1];
+    if (lang_name_index == 'js')
+      lang_name = 'javascript'
+    if (lang_name_index == 'md')
+      lang_name = 'markdown'
+    if (lang_name_index == 'plain')
+      lang_name = 'code'
+    if (lang_name_index == 'py')
+      lang_name = 'python'
+
+    // 首字母大写
+    lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1)
+
+    $('pre').siblings(".code_lang").text(lang_name)
+  })
 });
