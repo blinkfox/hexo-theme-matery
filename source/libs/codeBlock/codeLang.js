1ead97f41a641701d3b501c5f6c4d754f4edad0d
Licardo <1014660822@qq.com>
2019-10-14 17:14:28 +0800

    Update codeLang.js

--- source/libs/codeBlock/codeLang.js
+++ source/libs/codeBlock/codeLang.js
@@ -1,14 +1,14 @@
-// 代码框语言识别
+// 代码块语言识别
 
 $(function () {
-  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>')
-
-  $('pre').after($highlight_lang)
+  var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>');
+  
+  $('pre').after($highlight_lang);
   $('pre').each(function () {
     var lang_name = $('pre code').attr('class').split('-')[1];
 
     // 首字母大写
     lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-    $('pre').siblings(".code_lang").text(lang_name)
-  })
+    $('pre').siblings(".code_lang").text(lang_name);
+  });
 });
