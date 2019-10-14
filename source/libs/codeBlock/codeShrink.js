4f5874889edffd2ae3d462e30dfb922f73a51c2f
Licardo <1014660822@qq.com>
2019-10-14 17:14:50 +0800

    Update codeShrink.js

--- source/libs/codeBlock/codeShrink.js
+++ source/libs/codeBlock/codeShrink.js
@@ -1,9 +1,9 @@
-// 代码框收缩
+// 代码块收缩
 
 $(function () {
-  var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>')
+  var $code_expand = $('<i class="fas fa-angle-up code-expand" aria-hidden="true"></i>');
 
-  $('.code-area').prepend($code_expand)
+  $('.code-area').prepend($code_expand);
   $('.code-expand').on('click', function () {
     if ($(this).parent().hasClass('code-closed')) {
       $(this).siblings('pre').find('code').show();
@@ -12,5 +12,5 @@ $(function () {
       $(this).siblings('pre').find('code').hide();
       $(this).parent().addClass('code-closed');
     }
-  })
+  });
 });
