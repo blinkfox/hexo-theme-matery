d92a25e858fe276b9377f6156e50c2e9a0ad0f43
Licardo <1014660822@qq.com>
2019-10-14 13:36:44 +0800

    Update codeLang.js

--- source/libs/codeBlock/codeLang.js
+++ source/libs/codeBlock/codeLang.js
@@ -3,7 +3,7 @@ $(function () {
    * 代码框语言识别
    */
   var $highlight_lang = $('<div class="code_lang" title="代码语言"></div>')
-  $('pre').after($highlight_lang)
+  $('pre').before($highlight_lang)
   $('pre').each(function () {
     var lang_name = $('pre code').attr('class').split('-')[1];
 
