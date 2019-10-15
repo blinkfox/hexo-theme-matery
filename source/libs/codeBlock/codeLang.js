3391e6332cd02642af3f72e942a2b9e3ebebf4d8
Licardo <1014660822@qq.com>
2019-10-15 15:56:41 +0800

    Update codeLang.js

--- source/libs/codeBlock/codeLang.js
+++ source/libs/codeBlock/codeLang.js
@@ -5,8 +5,12 @@ $(function () {
 
   $('pre').after($highlight_lang);
   $('pre').each(function () {
-    var lang_name = $(this).attr('class').split('-')[2];
 
+    var code_language = $(this).attr('class');
+    if (!code_language || code_language.indexOf("-") === -1) {
+      return true;
+    }
+    var lang_name = code_language.split('-')[2];
     // 首字母大写
     lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
     $(this).siblings(".code_lang").text(lang_name);
