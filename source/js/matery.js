0c3f3cd3eb480e32369e4c05ce6ce00e385290f2
ialoe <ialoe@islu.cn>
2022-01-24 12:26:28 +0800

    适配Pjax和深色模式

--- source/js/matery.js
+++ source/js/matery.js
@@ -170,3 +170,51 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+/* 深色模式按钮设置 */
+if (localStorage.getItem("dark") === "1") {
+    document.body.classList.add("dark");
+} else {
+    /*定时开启暗色模式<默认晚22点至早6点默认开启>*/
+    if (new Date().getHours() >= 22 || new Date().getHours() < 6) {
+        document.body.classList.add("dark");
+        $("#nightMode").removeClass("fa-moon-o").addClass("fa-lightbulb");
+    } else {
+        if (matchMedia("(prefers-color-scheme: dark)").matches) {
+            document.body.classList.add("dark");
+        }
+    }
+}
+
+/*提醒开启功能*/
+setTimeout(function () {
+    if (
+        (new Date().getHours() >= 19 || new Date().getHours() < 7) &&
+        !$("body").hasClass("DarkMode")
+    ) {
+        let toastHTML =
+            '<span style="color:#97b8b2;border-radius: 10px;>' +
+            '<i class="fa fa-bell" aria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)/</span>';
+        M.toast({ html: toastHTML });
+    }
+}, 2200);
+
+/* 深色模式设置*/
+function switchNightMode() {
+    var body = document.body;
+    if (body.classList.contains("dark")) {
+        document.body.classList.remove("dark");
+        localStorage.setItem("dark", "0");
+        $("#nightMode").removeClass("fa-lightbulb").addClass("fa-moon");
+        return;
+    } else {
+        document.body.classList.add("dark");
+        localStorage.setItem("dark", "1");
+        $("#nightMode").removeClass("fa-moon").addClass("fa-lightbulb");
+        return;
+    }
+}
+
+/*friends-link*/
+$('#friends-link').masonry({
+    itemSelector: '.friend-div'
+});
