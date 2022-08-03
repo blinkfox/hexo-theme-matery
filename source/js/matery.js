1f5130fbab4ab3b2019e2d1d479dd868cdab77cd
appotry <andycrusoe@gmail.com>
2022-08-03 19:20:42 +0800

    添加 dark mode

--- source/js/matery.js
+++ source/js/matery.js
@@ -170,3 +170,20 @@ $(function () {
     // 初始化加载 tooltipped.
     $('.tooltipped').tooltip();
 });
+
+//黑夜模式提醒开启功能
+setTimeout(function () {
+    if ((new Date().getHours() >= 19 || new Date().getHours() < 7) && !$('body').hasClass('DarkMode')) {
+        let toastHTML = '<span style="color:#97b8b2;border-radius: 10px;>' + '<i class="fa fa-bellaria-hidden="true"></i>晚上使用深色模式阅读更好哦。(ﾟ▽ﾟ)</span>'
+        M.toast({ html: toastHTML })
+    }
+}, 2200);
+
+//黑夜模式判断
+if (localStorage.getItem('isDark') === '1') {
+    document.body.classList.add('DarkMode');
+    $('#sum-moon-icon').addClass("fa-sun").removeClass('fa-moon')
+} else {
+    document.body.classList.remove('DarkMode');
+    $('#sum-moon-icon').removeClass("fa-sun").addClass('fa-moon')
+}
